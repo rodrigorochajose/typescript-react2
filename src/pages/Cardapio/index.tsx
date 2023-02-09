598eea66d335f1ea006542868906fd16214040d5
Rodrigo Rocha <rodrigorochajose@hotmail.com>
2023-02-09 10:06:20 -0300

    Teste

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -12,7 +12,7 @@ export default function Cardapio() {
       </nav>
       <header className={styles.header}>
         <div className={styles.header__text}>
-          A casa do código e da massa
+          A casa do código e da massa TESTE
         </div>
       </header>
       <section>
